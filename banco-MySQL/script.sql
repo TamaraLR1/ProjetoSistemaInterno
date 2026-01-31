860c0b8f04db21020c25501e5af4a45299424e07
Tamara de Lima Rodrigues <r.tamaradelima1@gmail.com>
2026-01-31 15:19:31 -0300

    Alterado a tabela produtos para mostrar a quantidade inicial do estoque e adicionado a tabela de estoque

--- banco-MySQL/script.sql
+++ banco-MySQL/script.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE projeto_sistema_interno;
 
+USE projeto_sistema_interno;
+
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     firstName VARCHAR(255) NOT NULL,
@@ -16,6 +18,7 @@ CREATE TABLE products (
     name VARCHAR(255) NOT NULL,
     description TEXT,
     price DECIMAL(10, 2) NOT NULL,
+    stock_quantity INT DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) -- Chave estrangeira para o usuário que cadastrou
 );
@@ -27,6 +30,18 @@ CREATE TABLE product_images (
     image_url VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
+);
+
+CREATE TABLE inventory_movements (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT NOT NULL,
+    user_id INT NOT NULL, -- Vendedor que realizou a movimentação
+    quantity INT NOT NULL, -- Qtd movimentada (ex: 10 para entrada, -2 para saída)
+    movement_type ENUM('IN', 'OUT') NOT NULL, -- IN = Entrada, OUT = Saída
+    reason VARCHAR(255) DEFAULT 'Ajuste manual', -- Motivo: 'Cadastro Inicial', 'Venda', 'Reposição'
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 -- O comando 'ON DELETE CASCADE' garante que ao deletar um produto, todas as suas imagens relacionadas sejam deletadas.product_images
